42e02f62ff892d1c2de69278b7f9906ed46f6bad
cheersphilip <mail@philipwatson.co.uk>
2012-04-23 21:38:56 +0200

    Update js/game.js

--- js/game.js
+++ js/game.js
@@ -31,7 +31,7 @@ monsterImage.src = "images/monster.png";
 
 // Game objects
 var hero = {
-	speed: 256 // movement in pixels per second
+	speed: 50 // movement in pixels per second
 };
 var monster = {};
 var monstersCaught = 0;
@@ -60,16 +60,20 @@ var reset = function () {
 // Update game objects
 var update = function (modifier) {
 	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
+		hero.y -= hero.speed * modifier * 7;
+		hero.x += hero.speed * modifier * 4;
 	}
 	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
+		hero.y += hero.speed * modifier * 7;
+		hero.x -= hero.speed * modifier * 4;
 	}
 	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
+		hero.x -= hero.speed * modifier * 4;
+		hero.y -= hero.speed * modifier * 7;
 	}
 	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
+		hero.x += hero.speed * modifier * 4;
+		hero.y += hero.speed * modifier * 7;
 	}
 
 	// Are they touching?
